95d5af57ac31da8718b029ccf43adf535764adec
rubingerser <67050129+rubingerser@users.noreply.github.com>
2023-05-08 15:01:54 +0300

    Create aws_s3_bucket positive2.tf

---
+++ my-secrets/aws_s3_bucket positive2.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "positive2" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+  server_side_encryption_configuration  {
+    rule  {
+      apply_server_side_encryption_by_default  {
+        kms_master_key_id = "AKIAW3KOLCG3KO6test"
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    mfa_delete = true
+  }
+}
